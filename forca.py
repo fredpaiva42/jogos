a5cead71e710208e922e0a166bdf9a46384f9f17
Frederico Gomes de Paiva <fredericogdpaiva@gmail.com>
2021-08-31 17:04:40 -0300

    adicionando as letras certas a lista

--- forca.py
+++ forca.py
@@ -1,10 +1,28 @@
-
-
 def jogar():
     print("-----------------------------------")
     print("Bem vindo ao jogo da Forca!")
     print("-----------------------------------")
     print()
+
+    palavraSecreta = "banana"
+    letrasCertas = ["_"] * len(palavraSecreta)
+
+    enforcou = False
+    acertou = False
+
+    print(letrasCertas)
+
+    while (not enforcou and not acertou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavraSecreta:
+            if chute.upper() == letra.upper():
+                letrasCertas[index] = letra
+            index += 1
+        print(letrasCertas)
 
     print("Fim de Jogo!")
 
